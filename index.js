6a129eec9745a5a5541e1ad90201ee1bb702e8fb
geovanarochamp <geovana.rocha.martins@gmail.com>
2022-12-17 16:27:28 -0300

    list orders

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require("express")
-const port = 3000
 
+const port = 3000
 const app = express()
+app.use(express.json())
+
+/*
+
+{ id, order, clientName, price, status }
+
+{ 
+    id: "ac3ebf68-e0ad-4c1d-9822-ff1b849589a8", 
+    order: "X- Salada, 2 batatas grandes, 1 coca-cola", 
+    clientName:"José", 
+    price: 44.50, 
+    status:"Em preparação" 
+}
+
+*/
+
+const orders = []
 
 app.get("/orders", (request, response) => {
-  return response.send("Hello Node")
+  return response.json(orders)
 })
 
 app.listen(port, () => {
